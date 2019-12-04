62f63d9604117b18640afc9763399cb3ba8bc2ee
KrystianMigura <Krystian.migura@gmail.com>
2019-12-04 19:56:15 +0100

    server
    server update

--- server/createServer/Listener.cs
+++ server/createServer/Listener.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+
+
 namespace WindowsFormsApp1.server.createServer
 {
     public class thr
@@ -15,49 +17,95 @@ namespace WindowsFormsApp1.server.createServer
         public void setThread()
         {
             Listener works = new Listener();
-            
+
             Thread createSecondThread = new Thread(works.go);
             createSecondThread.Start();
         }
     }
-    
-    public class Listener 
+
+    public class Listener
     {
-       public Listener() { }
+        public Listener() { }
+
 
 
         public void go()
         {
+            IPAddress ipA = IPAddress.Parse("127.0.0.1");
+            TcpListener myList = new TcpListener(ipA, 10000);
+            myList.Start();
+            Byte[] bytes = new byte[256];
+            String data = null;
+            Console.WriteLine("server start o port 10000");
+            TcpClient client = myList.AcceptTcpClient();
+            Console.WriteLine("Connected!");
 
-            
+            NetworkStream stream = client.GetStream();
 
-           try
+            int i;
+
+            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
             {
-                IPAddress ipA = IPAddress.Parse("127.0.0.1");
+                // Translate data bytes to a ASCII string.
+                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                Console.WriteLine("Received: {0}", data);
 
-                TcpListener myListener = new TcpListener(ipA, 8001);
-                myListener.Start();
-                Console.WriteLine("server runing in port 8001");
-                Console.WriteLine("endpoint" + myListener.LocalEndpoint);
+                // Process the data sent by the client.
+                data = "to jest odpowiedz z servera do klienta wazne!!";
 
-                Socket s = myListener.AcceptSocket();
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
 
-                     byte[] b = new byte[100];
-                     int k = s.Receive(b);
-                     for(int i=0; i<k; i++)
-                         Console.Write(Convert.ToChar(b[i]));
+                // Send back a response.
+                stream.Write(msg, 0, msg.Length);
+                Console.WriteLine("Sent: {0}", data);
+            }
 
-                     ASCIIEncoding asen = new ASCIIEncoding();
-                     s.Send(asen.GetBytes("The string was recived by the server."));
-                     Console.WriteLine("Sent Acknowledgment");
+            // Shutdown and end connection
+            client.Close();
 
-                    s.Close();
-                    myListener.Stop();
-            }catch(Exception e)
+
+        }
+
+
+
+        /*
+       try
+        {
+            IPAddress ipA = IPAddress.Parse("127.0.0.1");
+
+            TcpListener myListener = new TcpListener(ipA, 8001);
+            myListener.Start();
+
+            Console.WriteLine("The server is running at port 8001...");
+            Console.WriteLine("The local End point is  :" +
+                               myListener.LocalEndpoint);
+            Console.WriteLine("Waiting for a connection.....");
+
+            Socket s = myListener.AcceptSocket();
+            Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+
+            byte[] b = new byte[100];
+            int k = s.Receive(b);
+            Console.WriteLine("Recieved...");
+            for (int i = 0; i < k; i++)
+                Console.Write(Convert.ToChar(b[i]));
+
+            ASCIIEncoding asen = new ASCIIEncoding();
+            s.Send(asen.GetBytes("The string was recieved by the server."));
+            Console.WriteLine("\nSent Acknowledgement");
+            /* clean up */
+        //s.Close();
+        //myListener.Stop();
+        /*
+            }
+            catch (Exception e)
             {
-                Console.WriteLine(e);
+            go();
+            Console.WriteLine("Error..... " + e.StackTrace);
             }
-            }
-            
+        */
     }
-}
+    }
+            
+    
+
